b092ec848d5efc73345d95b7be77a8ecb913ff1f
Juliana Sporrer <44637187+jusporrer@users.noreply.github.com>
2024-02-01 15:11:04 +0000

    Update test_all.R

--- test/test_all.R
+++ test/test_all.R
@@ -2,7 +2,7 @@ library(testthat)
 library(covr)
 
 # Calculate coverage
-coverage <- covr::file_coverage(source_files = "../main.R", test_files = "test/test_main.R")
+coverage <- covr::file_coverage(source_files = "main.R", test_files = "test/test_main.R")
 
 # Generate the report
 covr::report(coverage)
